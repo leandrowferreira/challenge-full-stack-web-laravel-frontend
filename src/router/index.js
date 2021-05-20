7076ca7596396c5ff528366a7008be5362848a7c
Leandro Ferreira <leandrowferreira@gmail.com>
2021-05-20 19:05:18 -0300

    Implements Student Edit route and view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import StudentIndex from '../views/StudentIndex.vue'
 import StudentCreate from '../views/StudentCreate.vue'
+import StudentEdit from '../views/StudentEdit.vue'
 
 Vue.use(VueRouter)
 
@@ -15,7 +16,12 @@ const routes = [
     path: '/create',
     name: 'StudentCreate',
     component: StudentCreate
-  }
+  },
+  {
+    path: '/:id/edit',
+    name: 'StudentEdit',
+    component: StudentEdit
+  },
 
 ]
 
---
+++ src/views/StudentEdit.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="create">
+        <h1 class="subtitle-1 grey--text">Editar aluno</h1>
+
+            <student-form
+                :ra="form.ra"
+                :cpf="form.cpf"
+                :name="form.name"
+                :email="form.email"
+                @cancel="gotoIndex"
+                @save="trySubmit"
+            />
+
+        <div class="student-edit">
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    import StudentForm from '../components/StudentForm'
+
+    export default {
+
+        components: {StudentForm},
+
+        data: () => ({
+
+            form: {
+                ra: '',
+                cpf: '',
+                name: '',
+                email: '',
+            },
+        }),
+
+
+        mounted() {
+            console.log(this.$route.params.id)
+
+
+        },
+
+
+
+        methods: {
+
+            gotoIndex() {
+                this.$router.push('/')
+            },
+
+            trySubmit(form) {
+                console.log(form)
+            }
+
+        },
+    }
+
+</script>
