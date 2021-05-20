417a21e7311c913714dbf6b33b296ed13975d767
Leandro Ferreira <leandrowferreira@gmail.com>
2021-05-20 15:45:33 -0300

    Adds search ability and create button

--- src/views/StudentIndex.vue
+++ src/views/StudentIndex.vue
@@ -13,6 +13,28 @@
                 class="elevation-1"
             >
 
+                <template v-slot:top>
+                    <v-layout wrap justify-space-between>
+                        <v-flex xs12 md10>
+                            <v-text-field v-model="search" label="Pesquisar" class="mx-4"></v-text-field>
+                        </v-flex>
+
+                        <v-flex xs12 md2 class="text-right">
+
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn class="mx-4 my-4" to="/create" color="primary" fab large dark v-bind="attrs" v-on="on">
+                                        <v-icon>mdi-account-plus</v-icon>
+                                    </v-btn>
+                                </template>
+                                <span>Cadastrar aluno</span>
+                            </v-tooltip>
+
+                        </v-flex>
+
+                    </v-layout>
+                </template>
+
             </v-data-table>
         </v-container>
 
@@ -25,6 +47,10 @@
     export default {
         data: () => ({
 
+            //The search filter
+            search: '',
+
+            //Student's array
             students: [
                 {id: 1,ra: '9876', name: 'Lorem ipsum dolor',     cpf: '123.456.789-10'},
                 {id: 2,ra: '9877', name: 'Ut arcu purus maximus', cpf: '124.457.790-11'},
