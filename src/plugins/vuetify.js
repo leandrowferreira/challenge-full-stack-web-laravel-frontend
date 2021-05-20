312c16808141e553619fed19a9e4f3783dfe562d
Leandro Ferreira <leandrowferreira@gmail.com>
2021-05-20 16:20:01 -0300

    Translates Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,12 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
+import pt from 'vuetify/src/locale/pt.ts'
+import en from 'vuetify/src/locale/en.ts'
+
 export default new Vuetify({
+    lang: {
+        locales: { pt, en },
+        current: 'pt',
+    },
 });
